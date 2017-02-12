64573fded29d93650256687983d9d1e7cdbf75e8
Thomas Roh <thmsroh@gmail.com>
2017-02-12 17:31:33 -0600

    corrected interval lookup in probability functions

--- R/fit_empirical_continuous.R
+++ R/fit_empirical_continuous.R
@@ -1,34 +1,62 @@
-library(stringr)
-x <- rpois(100, 5)
+#' Fit Continous Empirical Distribution Family
+#'
+#' @param x
+#'
+#' numeric vector
+#'
+#' @return
+#'
+#' family of probability distribution functions
+#'
+#' @export
+#'
+#' @examples
+#' set.seed(562)
+#` x <- rexp(100, 5)
+#` empCont <- fit_empirical_continuous(x)
+#` empCont$d(.2)
+#` empCont$p(.2)
+#` empCont$q(.8)
+#` empCont$r(100)
 fit_empirical_continuous <- function(x) {
   stopifnot(is.double(x))
   x <- sort(x)
-  binwidth <- diff(range(x)) / (2 * IQR(x) / length(x)^(1/3))
-  bins <- sapply(split(x, cut(x, binwidth)), length)
-  values <- calc_mid(names(bins))
+  # Freedman-Diaconis rule for optimal number of histogram bins
+  nbins <- diff(range(x)) / (2 * IQR(x) / length(x)^(1/3))
+  bins <- sapply(split(x, cut(x, nbins)), length)
+  intervals <- get_interval_nums(names(bins))
+  # use midpoint of intervals for return values
+  # small rounding errors
+  mids <- sapply(intervals, mean)
+  leftEnds <- sapply(intervals, min)
+  rightEnds <- sapply(intervals, max)
   probs <- bins/sum(bins)
 
   d <- function(x) {
-    probs[x == values]
+    if ( x > max(rightEnds) | x < min(leftEnds)) {
+      0
+    } else {
+      probs[x >= leftEnds & x < rightEnds]
+    }
   }
 
   p <- function(q) {
-    sum(probs[q <= values])
+    sum(probs[q >= leftEnds])
   }
 
-  q <- function(p) {
-    max(values[cumsum(probs) <= p])
+  qt <- function(p) {
+    max(mids[cumsum(probs) <= p])
   }
 
   r <- function(n) {
-    sample(x = values, size = n, prob = probs, replace = TRUE)
+    sample(x = mids, size = n, prob = probs, replace = TRUE)
   }
   list(d = d, p = p, q = q, r= r)
-
 }
 
-calc_mid <- function(cuts) {
-  nums <- strsplit(gsub('\\(|\\]|\\)|\\]', "", cuts), ",")
-  sapply(nums, function(x) mean(as.numeric(x)))
+get_interval_nums <- function(cuts) {
+  numChars <- strsplit(gsub('\\(|\\]|\\)|\\]', "", cuts), ",")
+  lapply(numChars, as.numeric)
 }
+
 
