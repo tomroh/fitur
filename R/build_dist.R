3c3cf36cad61bf4ee9df53b520022e6105d0cd31
Thomas Roh <thmsroh@gmail.com>
2017-02-13 21:35:36 -0600

    documentation and formatting. decided not to include optional paramater specification since it will lead to unintelligible errors. better to do this manually.

--- R/build_dist.R
+++ R/build_dist.R
@@ -1,6 +1,6 @@
 #' Build Distribution Functions
 #'
-#' An automated wrapper for building function families given
+#' A wrapper for building function families given
 #' a numeric vector and the distribution family
 #'
 #' @param x
@@ -32,20 +32,16 @@
 
 
 build_dist <- function(x, family) {
-
   # generate list of distribution functions
   type <- c('d', 'p', 'q', 'r')
   funs <- lapply(type, function(type) {
     get(paste0(type, family))
   })
   names(funs) <- type
-
   params <- fitdist(data = x, distr = family)[['estimate']]
   lapply(setNames(funs, names(funs)), gen_dist_fun,
          parameters = params)
 }
-#TODO optional parameter specification
-
 
 #' Generate Single Distribution Function
 #'
@@ -68,15 +64,3 @@ gen_dist_fun <- function(f, parameters, ...) {
   function(...)
     do.call(f, c(list(...), parameters))
 }
-
-# fittedDists <- build_dist(rpois(100,5), 'pois')
-# dpois(x = 5, lambda = 5)
-# fittedDists$d(5)
-# ppois(5, 5)
-# fittedDists$p(5)
-# qpois(.5, 5)
-# fittedDists$q(.5)
-# set.seed(82)
-# rpois(100, 5)
-# set.seed(82)
-# fittedDists$r(100)
