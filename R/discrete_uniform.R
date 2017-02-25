47a17012531eb0e39ca8a26c1ea666f7c929e854
Thomas Roh <thomas.roh@hdrinc.com>
2017-02-25 11:50:43 -0600

    added discrete uniform distribution. still need to make documentation consistent with distribution family docs

--- R/discrete_uniform.R
+++ R/discrete_uniform.R
@@ -1,40 +1,72 @@
-#
-# dunifdisc<-function(x, min=0, max=1) {
-#   ifelse(x>=min & x<=max & round(x)==x, 1/(max-min+1), 0)
-# }
-# punifdisc<-function(q, min=0, max=1) {
-#   ifelse(q<min, 0, ifelse(q>=max, 1, (floor(q)-min+1)/(max-min+1)))
-# }
-# qunifdisc<-function(p, min=0, max=1) {
-#   floor(p*(max-min+1))
-# }
-# runifdisc<-function(n, min=0, max=1) {
-#   sample(min:max, n, replace=T)
-# }
-#
-#
-# fit_uniform_discrete <- function(x) {
-#   stopifnot(is.integer(x))
-#   x <- sort(x)
-#   values <- unique(x)
-#   # convert to factors so tabulate doesn't ignore numbers <= 0
-#   probs <- tabulate(as.factor(x))/length(x)
-#
-#   d <- function(x) {
-#     probs[x == values]
-#   }
-#
-#   p <- function(q) {
-#     sum(probs[q <= values])
-#   }
-#
-#   q <- function(p) {
-#     max(values[cumsum(probs) <= p])
-#   }
-#
-#   r <- function(n) {
-#     sample(x = values, size = n, prob = probs, replace = TRUE)
-#   }
-#   list(d = d, p = p, q = q, r= r)
-#
-# }
+#' The Discrete Uniform Distribution
+#'
+#' @param x
+#'
+#' vector of (non-negative integer) quantiles
+#'
+#' @param q
+#'
+#' vector of quantiles
+#'
+#' @param p
+#'
+#' vector of probabilities
+#'
+#' @param n
+#'
+#' number of random values to return
+#'
+#' @param min
+#'
+#' minimum value of distribution (integer)
+#'
+#' @param max
+#'
+#' maximum value of distribution (integer)
+#'
+#' @return
+#'
+#' ddfunif gives the density, pdunif gives the distribution function,
+#' qdunif gives the quantile function, rdunif generates random deviates
+#'
+#' @export
+#'
+#' @examples
+#' ddunif(0:1)
+#' pdunif(1)
+#' qdunif(.5)
+#' rdunif(10)
+
+#'
+#' @export
+#'
+#' @rdname discrete_uniform
+#'
+ddunif <- function(x, min=0, max=1) {
+  ifelse(x>=min & x<=max & round(x)==x, 1/(max-min+1), 0)
+}
+
+#'
+#' @export
+#'
+#' @rdname discrete_uniform
+#'
+pdunif <- function(q, min=0, max=1) {
+  ifelse(q<min, 0, ifelse(q>=max, 1, (floor(q)-min+1)/(max-min+1)))
+}
+
+#'
+#' @export
+#'
+#' @rdname discrete_uniform
+qdunif <- function(p, min=0, max=1) {
+  floor(p*(max-min+1))
+}
+
+#'
+#' @export
+#'
+#' @rdname discrete_uniform
+rdunif <- function(n, min=0, max=1) {
+  sample(min:max, n, replace=TRUE)
+}
