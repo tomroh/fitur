8f437c5364593aa5516a38313f7d8961bba502c0
troh <thmsroh@gmail.com>
2018-07-16 18:16:20 -0500

    refactoring

--- R/utils.R
+++ R/utils.R
@@ -23,55 +23,6 @@ calc_moments <- function(x) {
     kurtosis = kurtosis(x))
 }
 
-#' Fit Univariate Distributions by Specifying Parameters
-#'
-#' @param distribution
-#'
-#' distribution character name
-#'
-#' @param parameters
-#'
-#' named vector of parameters to set
-#'
-#' @return
-#'
-#' list of distribution functions for d, p, q, r, and parameters
-#'
-#' @import stats
-#'
-#' @export
-#'
-#' @examples
-#' manFun <- fit_univariate_man('norm', c(mean = 2, sd = 5))
-#' set.seed(5)
-#' m1 <- mean(manFun$rnorm(100000))
-#' set.seed(5)
-#' m2 <- mean(rnorm(100000, 2, 5))
-#' identical(m1, m2)
-fit_univariate_man <- function(distribution, parameters) {
-
-  type <- paste0(c('d', 'p', 'q', 'r'), distribution)
-  funs <- lapply(type, function(type) {
-    match.fun(type)
-  })
-  names(funs) <- type
-
-  # parameter name checking
-  allParams <- unique(names(unlist(lapply(unname(funs), formals))))
-  specParams <- names(parameters)
-  matchedArgs <- match.arg(specParams, allParams, several.ok = TRUE)
-  if (length(matchedArgs) != length(specParams)) {
-    stop("Specified names of parameters do not match argument names of functions")
-  }
-
-  funs <- lapply(setNames(funs, names(funs)),
-                 gen_dist_fun,
-                 parameters = parameters)
-  funs[['parameters']] <- parameters
-  funs
-
-}
-
 #' Find Mode
 #'
 #' @param x
@@ -85,28 +36,6 @@ fit_univariate_man <- function(distribution, parameters) {
 Mode <- function(x) {
   ux <- unique(x)
   ux[which.max(tabulate(match(x, ux)))]
-}
-
-#' Generate Single Distribution Function
-#'
-#' @param f
-#'
-#' one of distribution functions
-#'
-#' @param parameters
-#'
-#' new parameters for distribution
-#'
-#' @param ...
-#'
-#' arguments to pass on to distribution function
-#'
-#' @return
-#' one of parameterized distribution functions in d, p, q, r
-
-gen_dist_fun <- function(f, parameters, ...) {
-  function(...)
-    do.call(f, c(list(...), parameters))
 }
 
 #' Q-Q Plot
