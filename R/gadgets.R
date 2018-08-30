9e8f997c7a7332b98032f27268cf9cfa6b001393
troh <thmsroh@gmail.com>
2018-08-30 06:54:06 -0500

    added reading variable from envir

--- R/gadgets.R
+++ R/gadgets.R
@@ -1,12 +1,23 @@
-fitDistributionsAddin <- function() {
+#' Fit Univariate Distributions Addin
+#'
+#' Interactively submit a numeric vector and choose what distributions that
+#' you want to run fit diagnostics. Click done to have the desired distribution
+#' code put into your cursor position.
+#'
+#'
+#'
+#' @export
+fit_dist_addin <- function() {
   ui <- miniPage(
     gadgetTitleBar("Drag to select points"),
     miniContentPanel(
       fillRow(
         fillCol(flex = c(NA, NA, 1, 1),
           selectInput('distributions', 'Distributions',
-                      choices = c('Weibull', 'Lognormal')),
-          numericInput('data', label = 'Data', value = 1),
+                      choices = c('Weibull', 'Lognormal'),
+                      multiple = TRUE),
+          selectInput('data', label = 'Data',
+                    choices = 'x'),#ls()[sapply(ls(), function(xChar) is.numeric(get(xChar)))]),
           DTOutput('gofTable'),
           plotOutput('ppPlot', height = '100%')
         ),
@@ -14,6 +25,7 @@ fitDistributionsAddin <- function() {
           numericInput('nbins', 'No. of Bins', value = 30,
                        min = 5, max = 100, step = 1),
           plotOutput('densityPlot', height = '100%'),
+          #textOutput('whatX'),
           plotOutput('qqPlot', height = '100%')
           )
       )
@@ -22,13 +34,21 @@ fitDistributionsAddin <- function() {
 
   server <- function(input, output, session) {
     require(fitur)
-    x <- rweibull(100, 1)
+    # x <- rweibull(100, 1)
+    # xChar <- sapply(ls(), function(xChar) is.numeric(get(xChar)))
+    # whatX <- renderText({print(x())})
+    x <- reactive({
+      print(get(input$data))
+      get(input$data, envir = .GlobalEnv)
+    })
     fits <- reactive({
+      x <- x()
       dists <- c('gamma', 'lnorm', 'weibull')
       fits <- lapply(dists, fit_univariate, x = x)
     })
     output$gofTable <- renderDT({
-      datatable(gof_tests(fits(), x),
+      gof <- gof_tests(fits(), x())
+      datatable(gof,
                 options = list(searching = FALSE,
                                lengthMenu = -1,
                                lengthChange = FALSE,
@@ -37,17 +57,17 @@ fitDistributionsAddin <- function() {
 
     })
     output$densityPlot <- renderPlot({
-      plot_density(x, fits(), input$nbins) +
+      plot_density(x(), fits(), input$nbins) +
         theme_bw()
     })
 
     output$ppPlot <- renderPlot({
-      plot_pp(x, fits()) +
+      plot_pp(x(), fits()) +
         theme_bw()
     })
 
     output$qqPlot <- renderPlot({
-      plot_qq(x, fits()) +
+      plot_qq(x(), fits()) +
         theme_bw()
     })
     observeEvent(input$done, {
